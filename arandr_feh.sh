94fd7d11adbb6ce489d4d4d2d0641eb42fb5d5a9
Sat <78806874+stking68@users.noreply.github.com>
2022-06-25 18:43:25 +0430

    Add files via upload

---
+++ arandr_feh.sh
@@ -0,0 +1,36 @@
+# OPTIONS
+first() {
+xrandr --output VGA-0 --off --output LVDS-0 --off --output DVI-D-0 --off --output DVI-D-1 --mode 1920x1080 --pos 1920x0 --rotate normal --output DP-0 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-1 --off --output DP-2 --off --output DP-3 --off
+}
+
+two() {
+feh --bg-scale /home/satyar/Pictures/streets_grayscale_3888x2592.jpg
+}
+
+three() {
+break;
+}
+
+# MAIN
+lines='---------------------------------'
+echo
+echo $lines
+echo "Which one do you want?"
+echo '1) Arandr'
+echo '2) feh'
+echo '3) Quit'
+echo $lines
+
+read $choice
+
+case $choice in
+	("2")
+		two
+		;;
+	("3")
+		three
+		;;
+	("1" | *)
+		first
+		;;
+esac
