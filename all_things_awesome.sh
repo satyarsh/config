5ecd5841697a5ab886898e168599479aeb4edde4
Satyar Sh <78806874+satyarsh@users.noreply.github.com>
2024-09-01 14:08:30 +0330

    Update all_things_awesome.sh

--- all_things_awesome.sh
+++ all_things_awesome.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+### Script to open AwesomeWM config files
+### i haven't used it much since 2022 so take it as is ...
+
 #script to open all of the awesome config files 
 codium .config/awesome/rc.lua
 codium .config/awesome/autorun.sh
